9fa9c49a4d7d622dad0a739bdb66ef8b272a9abf
Daniel Bonfim <158781988+danielbonfim23@users.noreply.github.com>
2024-06-30 12:41:06 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1719761892,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/caixa-sheet0.png",
+		"images/caixa-sheet1.png",
+		"images/caixa-sheet2.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/pincel-sheet0.png",
+		"images/cores__cor-sheet0.png",
+		"images/cores__cor-sheet1.png",
+		"images/cores__cor-sheet2.png",
+		"images/barreiras-sheet0.png",
+		"images/objetos-sheet0.png",
+		"images/objetos-sheet1.png",
+		"images/praia-sheet0.png",
+		"images/confirmar_compra-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"acess_codes2.xml"
+	]
+}
